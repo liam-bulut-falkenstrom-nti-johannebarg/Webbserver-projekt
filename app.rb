c35bffc3e8f0c68a5647472aad5b5e20a27f6756
liam-bulut-falkenstrom-nti-johannebarg <liam.bulutfalkenstrom@elev.ga.ntig.se>
2022-04-14 22:02:51 +0200

    lagt till att man kan lägga skapa nya teams och att de kan visas på hemsidan

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ get('/')  do
     pokemon_array_hash = []
     db = SQLite3::Database.new('db/database.db')
     db.results_as_hash = true
-    if filter == "all"
+    if filter == "all" || filter == nil
         pokemon_array_hash = db.execute("SELECT * FROM pokemon")
     else
         type_id = db.execute("SELECT id FROM type WHERE type_name = ?", filter).first["id"]
@@ -19,7 +19,7 @@ get('/')  do
             pokemon_array_hash << db.execute("SELECT * FROM pokemon WHERE id = ?", pkmn_id_hash["pkmn_id"]).first
         end
     end
-    slim(:pokemons, locals:{logged_in_user: session[:id], pokemon_array_hash: pokemon_array_hash}) #Kanske låta session innehålla username också?
+    slim(:pokemons, locals:{logged_in_user: session[:id], added_pokemon_id_array: session[:added_pkmns], pokemon_array_hash: pokemon_array_hash}) #Kanske låta session innehålla username också?
 end 
 
 post('/users/new') do
@@ -39,8 +39,63 @@ post('/users/new') do
     end
 end
 
+get('/pokemons/new') do
+    slim(:"pokemons/new", locals:{logged_in_user: session[:id], added_pokemon_id_array: session[:added_pkmns]})
+end
+
+post('/pokemons') do
+    team_name = params[:team_name]
+    added_pokemon_id_array = session[:added_pkmns]
+    db = SQLite3::Database.new('db/database.db')
+    p session[:id]
+    p team_name
+    p added_pokemon_id_array
+    db.execute("INSERT INTO team (team_name, user_id) VALUES (?,?)", team_name, session[:id])
+    team_id = db.execute("SELECT id FROM team WHERE team_name = ?", team_name).first[0]
+    p team_id
+    added_pokemon_id_array.each do |added_pokemon_id|
+        db.execute("INSERT INTO team_pkmn_relation (team_id, pkmn_id) VALUES (?,?)", team_id, added_pokemon_id)
+    end
+    session[:added_pkmns] = nil
+    redirect('/')
+end
+
+get('/teams') do
+
+    user_id = session[:id]
+    p user_id
+    if user_id == nil 
+        slim(:teams, locals:{logged_in_user: user_id, added_pokemon_id_array: nil, team_hash_array: nil, array_with_all_team_pokemon_hashes: nil})
+    else
+        db = SQLite3::Database.new('db/database.db')
+        db.results_as_hash = true
+        username = db.execute("SELECT username FROM users WHERE id = ?", user_id).first["username"]
+        p username
+        team_hash_array = db.execute("SELECT id, team_name FROM team WHERE user_id = ?", user_id)
+        p team_hash_array
+           
+        team_pokemon_id_hash_array = []
+        team_hash_array.each do |team_hash|
+            team_pokemon_id_hash_array << db.execute("SELECT pkmn_id FROM team_pkmn_relation WHERE team_id = ?", team_hash["id"])
+        end        
+
+        array_with_all_team_pokemon_hashes = []
+        team_pokemon_id_hash_array.each do |array_with_team_pokemon_ids|
+            temp_array = []
+            array_with_team_pokemon_ids.each do |pokemon_hash|
+                temp_array << db.execute("SELECT * FROM pokemon WHERE id = ?", pokemon_hash["pkmn_id"]).first
+            end
+            array_with_all_team_pokemon_hashes << temp_array
+        end
+
+        p array_with_all_team_pokemon_hashes
+        slim(:teams, locals:{logged_in_user: username, added_pokemon_id_array: session[:added_pkmns], team_hash_array: team_hash_array, array_with_all_team_pokemon_hashes: array_with_all_team_pokemon_hashes})
+        
+    end
+end 
+
 get('/register')  do
-    slim(:register, locals:{logged_in_user: session[:id]})
+    slim(:register, locals:{logged_in_user: session[:id], added_pokemon_id_array: session[:added_pkmns]})
 end 
 
 post('/login') do
@@ -63,11 +118,24 @@ post('/login') do
 end
 
 get('/login')  do
-    slim(:login, locals:{logged_in_user: session[:id]}) #Måste man ha locals på alla??
+    slim(:login, locals:{logged_in_user: session[:id], added_pokemon_id_array: session[:added_pkmns]}) #Måste man ha locals på alla??
 end 
 
 get('/destroy') do
     session.destroy
+    redirect('/')
+end
+
+get('/add/:pkmn_id') do
+    if session[:added_pkmns] == nil
+        session[:added_pkmns] = []
+    end
+    session[:added_pkmns] << params[:pkmn_id].to_i
+    redirect('/')
+end
+
+get('/cancel') do
+    session[:added_pkmns] = nil
     redirect('/')
 end
 
@@ -81,5 +149,5 @@ get('/pokemon/:id') do
     end
     
     pokemon_hash = db.execute("SELECT * FROM pokemon WHERE id = ?", params[:id]).first # Finns det bättre sätt att göra detta på?
-    slim(:show, locals:{logged_in_user: session[:id], pokemon_hash: pokemon_hash, type_hash_array: type_hash_array})
+    slim(:show, locals:{logged_in_user: session[:id], pokemon_hash: pokemon_hash, type_hash_array: type_hash_array, added_pokemon_id_array: session[:added_pkmns]})
 end
