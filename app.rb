ca05628509632b89a1e01eb02243473d63240e59
liam-bulut-falkenstrom-nti-johannebarg <liam.bulutfalkenstrom@elev.ga.ntig.se>
2022-04-14 14:53:22 +0200

    lagt till filter funktion för att lättare kunna söka efter pokemons

--- app.rb
+++ app.rb
@@ -6,9 +6,19 @@ require 'bcrypt'
 enable :sessions
 
 get('/')  do
+    filter = params[:filter] # Lägga till filter grejer här tackkkkkkkkkkkkkk
+    pokemon_array_hash = []
     db = SQLite3::Database.new('db/database.db')
     db.results_as_hash = true
-    pokemon_array_hash = db.execute("SELECT * FROM pokemon")
+    if filter == "all"
+        pokemon_array_hash = db.execute("SELECT * FROM pokemon")
+    else
+        type_id = db.execute("SELECT id FROM type WHERE type_name = ?", filter).first["id"]
+        pkmn_id_hash_array = db.execute("SELECT pkmn_id FROM pkmn_type_relation WHERE type_id = ?", type_id)
+        pkmn_id_hash_array.each do |pkmn_id_hash|
+            pokemon_array_hash << db.execute("SELECT * FROM pokemon WHERE id = ?", pkmn_id_hash["pkmn_id"]).first
+        end
+    end
     slim(:pokemons, locals:{logged_in_user: session[:id], pokemon_array_hash: pokemon_array_hash}) #Kanske låta session innehålla username också?
 end 
 
@@ -19,13 +29,13 @@ post('/users/new') do
     
     db = SQLite3::Database.new('db/database.db')
     if db.execute("SELECT username FROM users WHERE username = ?", username).first != nil
-        "användarnamnet finns redan" #Undrar om man får använda sessions här eller finns det smartare lösning???
+        "användarnamnet finns redan" #Undrar om man får använda sessions här eller finns det smartare lösning??? Kanske skicka till error screen bara
     elsif confirmed_password == password
         password_digest = BCrypt::Password.create(confirmed_password)
         db.execute("INSERT INTO users (username, password_digest) VALUES (?,?)", username, password_digest)
         redirect('/register')
     else
-        "lösenorden matchar ej" #Undrar om man får använda sessions här eller finns det smartare lösning???
+        "lösenorden matchar ej"
     end
 end
 
@@ -69,8 +79,6 @@ get('/pokemon/:id') do
     type_id_hash_array.each do |type_id_hash|
         type_hash_array << db.execute("SELECT type_name FROM type WHERE id = ?", type_id_hash["type_id"]).first
     end
-    p type_hash_array
-
     
     pokemon_hash = db.execute("SELECT * FROM pokemon WHERE id = ?", params[:id]).first # Finns det bättre sätt att göra detta på?
     slim(:show, locals:{logged_in_user: session[:id], pokemon_hash: pokemon_hash, type_hash_array: type_hash_array})
