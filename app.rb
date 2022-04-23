f1a693ec1b3bcd87738f17f1fbde55446adc3167
liam-bulut-falkenstrom-nti-johannebarg <liam.bulutfalkenstrom@elev.ga.ntig.se>
2022-04-23 16:28:24 +0200

    förbättrat DRY-kod

--- app.rb
+++ app.rb
@@ -8,7 +8,9 @@ enable :sessions
 
 include Model
 
-# Ska jag ha med update??? jaaaaaaa
+# Kan inte ha samma namn på teams?? kanske ändra i databasen
+# sparas routes i before block?
+
 
 before('/teams/') do
     if session[:logged_in_user] == nil 
@@ -26,20 +28,44 @@ end
 before('/teams') do
     if session[:logged_in_user] == nil
         redirect('/error/You_need_to_log_in_to_add_teams')
-    end
-    if session[:added_pkmns] == nil
+    elsif session[:added_pkmns] == nil
         redirect('/error/Please_choose_at_least_one_Pokemon')
-    end
-    if params[:team_name].length == 0
+    elsif params[:team_name].length == 0
         redirect('/error/Please_name_your_team')
     end
 end
 
-#kanske har alla errorchecks som before-block typ som register grejen
+before('/users/login_user') do
+    username = params[:username]
+    if username_validation(username) == nil
+        redirect('/error/User_does_not_exist')
+    end
+end
 
-get('/')  do
+before('/users/new') do
+    username = params[:username]
+    if username_validation(username) != nil
+        redirect('/error/Username_already_exists')
+    end
+end
+
+before('/teams/:id/update') do
+    new_team_name = params[:new_team_name]
+    team_id = params[:id]
+    username = get_user_name(team_id)
+    if session[:logged_in_user] == nil
+        redirect('/error/You_need_to_log_in_to_change_team_name')
+    elsif session[:logged_in_user] != username
+        redirect('/error/This_user_does_not_have_permission_to_change_name_of_this_team')
+    elsif new_team_name.length == 0
+        redirect('/error/Please_choose_a_name_for_your_team')
+    end
+end
+
+after all_of('/', '/users/login_user', '/destroy', '/cancel') do
     redirect('/pokemons/')
 end
+
 
 
 
@@ -51,9 +77,7 @@ get('/pokemons/')  do
     slim(:"/pokemons/index", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns], pokemon_array_hash: pokemon_array_hash})
 end 
 
-
 get('/pokemons/:id') do
-
     output_array = get_pokemons_and_types(params[:id])
 
     slim(:"/pokemons/show", locals:{username: session[:logged_in_user], pokemon_hash: output_array[0], type_hash_array: output_array[1], added_pokemon_id_array: session[:added_pkmns]})
@@ -67,11 +91,9 @@ get('/teams/') do
     slim(:"/teams/index", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns], user_hash_array: output_array[0], team_hash_nested_array: output_array[1], team_pokemon_name_hash_nested_array: output_array[2]})
 end 
 
-
 get('/teams/new') do
     slim(:"/teams/new", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns]})
 end
-
 
 get('/teams/:id/edit') do
     team_id = params[:id]
@@ -79,11 +101,10 @@ get('/teams/:id/edit') do
     slim(:"/teams/edit", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns], team_hash: team_hash})
 end
 
-post('/photos/:id/update') do
+post('/teams/:id/update') do
     team_id = params[:id]
     new_team_name = params[:new_team_name]
     update_team(team_id, new_team_name)
-
     redirect('/teams/')
 end
 
@@ -92,33 +113,31 @@ post('/teams') do
     added_pokemon_id_array = session[:added_pkmns]
 
     session[:added_pkmns] = new_team(team_name, added_pokemon_id_array, session[:logged_in_user])
-
     redirect('/teams/')
 end
 
 
 post('/teams/:id/delete') do
     team_id = params[:id].to_i
-
     delete_team(team_id)
-
     redirect('/teams/')
 end
 
 
 
 get('/users/login')  do
-    slim(:"/users/login", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns]}) #Måste man ha locals på alla??
+    slim(:"/users/login", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns]})
 end
 
 
-post('/users/login') do    
+post('/users/login_user') do    
     username = params[:username]
     password = params[:password]
 
     session[:logged_in_user] = login_user(username, password)
-
-    redirect('/pokemons/')
+    if session[:logged_in_user] == nil
+        redirect('/error/Wrong_password')
+    end
 end
 
 
@@ -132,17 +151,17 @@ post('/users/new') do
     password = params[:password]
     confirmed_password = params[:confirmed_password]
 
-    register_user(username, password, confirmed_password)
-
-    redirect('/users/new')
-
+    if register_user(username, password, confirmed_password) == nil
+        redirect('/error/Passwords_do_not_match')
+    else
+        redirect('/users/new')
+    end
 end
 
 
 
 get('/destroy') do
     session.destroy
-    redirect('/pokemons/')
 end
 
 
@@ -157,7 +176,6 @@ end
 
 get('/cancel') do
     session[:added_pkmns] = nil
-    redirect('/pokemons/')
 end
 
 
--- model.rb
+++ model.rb
@@ -7,40 +7,56 @@
 # får man redirecta i model.rb?
 
 module Model
-    
+    def all_of(*strings)
+        return /(#{strings.join("|")})/
+    end
+       
+
+
     def connect_to_db(path)
         db = SQLite3::Database.new(path)
         db.results_as_hash = true
         return db
     end
 
-    def login_user(username, password)
+
+
+    def username_validation(username)
         db = connect_to_db('db/database.db')
         users_hash = db.execute("SELECT * FROM users WHERE username = ?", username).first
         if users_hash == nil
-            redirect('/error/User_does_not_exist')
-        else
-            user_id = users_hash["id"]
-            digested_password = users_hash["password_digest"]
-            if BCrypt::Password.new(digested_password) == password
-                return username
-            else
-                redirect('/error/Wrong_password')
-            end
+            return nil
+        else 
+            return username
         end
     end
 
-    def register_user(username, password, confirmed_password)
+
+    def login_user(username, password)
         db = connect_to_db('db/database.db')
-        if db.execute("SELECT username FROM users WHERE username = ?", username).first != nil
-            redirect('/error/Username_already_exists')
-        elsif confirmed_password == password
-            password_digest = BCrypt::Password.create(confirmed_password)
-            db.execute("INSERT INTO users (username, password_digest) VALUES (?,?)", username, password_digest)
+        users_hash = db.execute("SELECT * FROM users WHERE username = ?", username).first
+
+        user_id = users_hash["id"]
+        digested_password = users_hash["password_digest"]
+        if BCrypt::Password.new(digested_password) == password
+            return username
         else
-            redirect('/error/Passwords_do_not_match')
+            return nil
         end
     end
+    
+
+    def register_user(username, password, confirmed_password)
+        db = connect_to_db('db/database.db')
+        if confirmed_password == password
+            password_digest = BCrypt::Password.create(confirmed_password)
+            db.execute("INSERT INTO users (username, password_digest) VALUES (?,?)", username, password_digest)
+            return username
+        else
+            return nil
+        end
+    end
+
 
     def filter_pokemon(filter)
         db = connect_to_db('db/database.db')
@@ -51,6 +67,7 @@ module Model
         end
     end
 
+
     def get_pokemons_and_types(pokemon_id)
         db = connect_to_db('db/database.db')
     
@@ -59,6 +76,8 @@ module Model
         pokemon_hash = db.execute("SELECT * FROM pokemon WHERE id = ?", pokemon_id).first
         return [pokemon_hash, type_hash_array]
     end
+
+
 
     def new_team(team_name, added_pokemon_id_array, user_name)
         db = connect_to_db('db/database.db')
@@ -71,23 +90,27 @@ module Model
         return nil
     end
 
+
     def delete_team(team_id)
         db = SQLite3::Database.new('db/database.db')
         db.execute("DELETE FROM team_pkmn_relation WHERE team_id = ?", team_id)
         db.execute("DELETE FROM team WHERE id = ?", team_id)
     end
 
+
     def update_team(team_id, new_team_name)
         db = SQLite3::Database.new('db/database.db')
         db.execute("UPDATE team SET team_name = ? WHERE id = ?", new_team_name, team_id)
     end
 
+
+
     def get_team_hash(team_id)
         db = connect_to_db('db/database.db')
         team_hash = db.execute("SELECT * FROM team WHERE id = ?", team_id).first
-        p team_hash
         return team_hash
     end
+
 
     def get_team(username)
         db = connect_to_db('db/database.db')
@@ -113,6 +136,7 @@ module Model
         return [user_hash_array, team_hash_nested_array, team_pokemon_name_hash_nested_array]
     end
 
+    
     def get_user_name(team_id)
         db = connect_to_db('db/database.db')
         user_name = db.execute("SELECT username FROM team INNER JOIN users ON users.id = team.user_id WHERE team.id = ?", team_id).first["username"]
