58f407083bf3d39b29d2f7c0e53654b27d8f71d4
liam-bulut-falkenstrom-nti-johannebarg <liam.bulutfalkenstrom@elev.ga.ntig.se>
2022-04-28 23:03:56 +0200

    färdig med yardoc

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ enable :sessions
 
 include Model
 
-# Checks if user is logged in or not
+# Checks if user is logged in. If not, redirects to '/error/:error_message'
 #
 before('/teams/') do
     if session[:logged_in_user] == nil 
@@ -16,8 +16,11 @@ before('/teams/') do
     end
 end
 
-# Checks if the correct user i sending a request to delete a team
-# 
+# Checks if the correct user is sending a request to delete a team. If not, redirects to '/error/:error_message'
+#
+# @param [Integer] team_id, id of team user wants to delete
+#
+# @see Model#get_user_name
 before('/teams/:id/delete') do
     user_name = get_user_name(params[:id])
     if user_name != session[:logged_in_user] && session[:logged_in_user] != "Admin"
@@ -25,6 +28,8 @@ before('/teams/:id/delete') do
     end
 end
 
+# Checks if user is logged in, if user has chosen any pokemon for the new team and if user has chosen a name for the team that is going to be created. If not, redirects to '/error/:error_message'
+#
 before('/teams') do
     if session[:logged_in_user] == nil
         redirect('/error/You_need_to_log_in_to_add_teams')
@@ -35,6 +40,11 @@ before('/teams') do
     end
 end
 
+# Checks if username exists in database as a user, checks if user is trying to log in too fast. If an error is detected, redirects to '/error/:error_message'
+#
+# @param [String] username, name of user trying to log in.
+#
+# @see Model#username_validation
 before('/users/login_user') do
     username = params[:username]
     if session[:logging] != nil
@@ -47,6 +57,11 @@ before('/users/login_user') do
     end
 end
 
+# Checks if username trying to be registered already exists in the database. If not, redirects to '/error/:error_message'
+#
+# @param [String] username, name of user requested to be registered.
+#
+# @see Model#username_validation
 before('/users/new') do
     username = params[:username]
     if username_validation(username) != nil
@@ -54,6 +69,11 @@ before('/users/new') do
     end
 end
 
+# Checks if user is logged in, if logged in user has permission to update the chosen team and if user has chosen a new name for the team. If not, redirects to '/error/:error_message'
+#
+# @param [String] username, name of user requested to be registered.
+#
+# @see Model#username_validation
 before('/teams/:id/update') do
     new_team_name = params[:new_team_name]
     team_id = params[:id]
@@ -67,10 +87,15 @@ before('/teams/:id/update') do
     end
 end
 
+# redirects to '/pokemons/' after requesting the specified routes
+#
 after all_of('/', '/destroy', '/cancel') do
     redirect('/pokemons/')
 end
 
+# logs users ip adress and time of logging in after user tries to log in and '/users/login_user' is requested
+#
+# @see Model#logging
 after('/users/login_user') do
     ip_address = @env['REMOTE_ADDR']
     time = Time.new
@@ -78,6 +103,11 @@ after('/users/login_user') do
     logging(ip_address, time_string)
 end
 
+# Displays pokemon stored in the database. Can be filtered by pokemon type
+#
+# @param [String] filter, name of type. Filters after type.
+#
+# @see Model#filter_pokemon
 get('/pokemons/')  do
     filter = params[:filter]
 
@@ -86,30 +116,49 @@ get('/pokemons/')  do
     slim(:"/pokemons/index", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns], pokemon_array_hash: pokemon_array_hash})
 end 
 
+# Displays a single pokemon stored in the database
+#
+# @param [Integer] pokemon_id, id of pokemon used to specifiy pokemon in database.
+#
+# @see Model#get_pokemons_and_types
 get('/pokemons/:id') do
     output_array = get_pokemons_and_types(params[:id])
 
     slim(:"/pokemons/show", locals:{username: session[:logged_in_user], pokemon_hash: output_array[0], type_hash_array: output_array[1], added_pokemon_id_array: session[:added_pkmns]})
 end
 
-
-
+# Displays user teams stored in the database. Displays all user's teams if user is logged in as Admin
+#
+# @see Model#get_team
 get('/teams/') do
     output_array = get_team(session[:logged_in_user])
 
     slim(:"/teams/index", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns], user_hash_array: output_array[0], team_hash_nested_array: output_array[1], team_pokemon_name_hash_nested_array: output_array[2]})
 end 
 
+# Displays a form where user names team they are adding to the database
+#
 get('/teams/new') do
     slim(:"/teams/new", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns]})
 end
 
+# Displays a form where user can choose new name for an owned team.
+#
+# @param [Integer] team_id, id of team that is going to be updated.
+#
+# @see Model#get_team_hash
 get('/teams/:id/edit') do
     team_id = params[:id]
     team_hash = get_team_hash(team_id)
     slim(:"/teams/edit", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns], team_hash: team_hash})
 end
 
+# Updates team name with the new team name
+#
+# @param [Integer] team_id, id of team that is going to be updated.
+# @param [String] new_team_name, id of team that is going to be updated.
+#
+# @see Model#update_team
 post('/teams/:id/update') do
     team_id = params[:id]
     new_team_name = params[:new_team_name]
@@ -117,28 +166,43 @@ post('/teams/:id/update') do
     redirect('/teams/')
 end
 
+# Adds new team in database
+#
+# @param [String] team_name, name of team that is going to be added.
+#
+# @see Model#new_team
 post('/teams') do
     team_name = params[:team_name]
     added_pokemon_id_array = session[:added_pkmns]
 
-    session[:added_pkmns] = new_team(team_name, added_pokemon_id_array, session[:logged_in_user])
+    new_team(team_name, added_pokemon_id_array, session[:logged_in_user])
+    session[:added_pkmns] = nil
     redirect('/teams/')
 end
 
-
+# Deletes a specified team record from database
+#
+# @param [Integer] team_id, id of team that is going to be deleted.
+#
+# @see Model#delete_team
 post('/teams/:id/delete') do
     team_id = params[:id].to_i
     delete_team(team_id)
     redirect('/teams/')
 end
 
-
-
+# Display a form used to log in
+#
 get('/users/login')  do
     slim(:"/users/login", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns]})
 end
 
-
+# Logs in user if password is correct and logs the time of logging in.
+#
+# @param [String] username, username user typed in
+# @param [String] password, password user typed in
+#
+# @see Model#login_user
 post('/users/login_user') do    
     username = params[:username]
     password = params[:password]
@@ -152,12 +216,19 @@ post('/users/login_user') do
    
 end
 
-
+# Displays a form where user can register an account
+#
 get('/users/new')  do
     slim(:"/users/new", locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns]})
 end 
 
-
+# Registers a new account if password and confirmed password match. If not it redirects to the '/error/:error_message' route
+#
+# @param [String] username, username user typed in
+# @param [String] password, password user typed in
+# @param [String] confirmed_password, same password user types in a second time to confirm their choice
+#
+# @see Model#register_user
 post('/users/new') do
     username = params[:username]
     password = params[:password]
@@ -171,12 +242,14 @@ post('/users/new') do
 end
 
 
-
+# removes all data stored in session from logged in user.
+#
 get('/destroy') do
     session.destroy
 end
 
-
+# Add a pokemon to the :added_pkmns array stored in sessions. Stores which pokemon user has choosen for a new team. Makes sure user can onlu choose a maximum of 6 pokekom per team.
+#
 get('/add/:pkmn_id') do
     if session[:added_pkmns] == nil
         session[:added_pkmns] = []
@@ -188,12 +261,16 @@ get('/add/:pkmn_id') do
     redirect('/pokemons/')
 end
 
-
+# removes data from the :added_pkmns array stored in sessions. This route is requested when a standard user cancels their current team they are creating.
+#
 get('/cancel') do
     session[:added_pkmns] = nil
 end
 
-
+# Displays an error message when an error occurs. A route includes the error message in the params when redirecting to this route
+#
+# @param [String] error_message, the string that is to be displayed on screen but with underscores instead of spaces.
+#
 get('/error/:error_message') do
     error_message = params[:error_message].split("_").join(" ")
     slim(:error, locals:{username: session[:logged_in_user], added_pokemon_id_array: session[:added_pkmns], error_message: error_message})
--- model.rb
+++ model.rb
@@ -1,29 +1,40 @@
-
-# Ska man ha alla alla funktioner här? 
-
-# alla databaser i model.rb
-# inga session i model.rb
-
-# får man redirecta i model.rb?
-
 module Model
+    # fetches the paths of multiple routes
+    #
+    # @param [String] routes Name of all chosen routes
+    #
+    # @return [String] containing all paths to chosen routes
     def all_of(*strings)
         return /(#{strings.join("|")})/
     end
-       
+
+    # Inserts time of login and ip-address of user into database
+    #
+    # @param [String] time Time and date of login try
+    # @param [String] ip Ip-adress of user
+    #       
     def logging(time, ip_address)
         db = SQLite3::Database.new('db/database.db')
         db.execute("INSERT INTO logging (ip, time) VALUES (?,?)", ip_address, time)
     end
-
+    
+    # creates a variable and connects it to the database
+    #
+    # @param [String] path The path to the database, in this case: 'db/database.db'
+    #
+    # @return [Database] accessed through created variable 'db'
     def connect_to_db(path)
         db = SQLite3::Database.new(path)
         db.results_as_hash = true
         return db
     end
 
-
-
+    # Checks if username already exists in the database
+    #
+    # @option params [String] username The name of the account the user put in to register/login
+    #
+    # @return [String] 
+    # @return [nil] if name does not exist in database already
     def username_validation(username)
         db = connect_to_db('db/database.db')
         users_hash = db.execute("SELECT * FROM users WHERE username = ?", username).first
@@ -35,6 +46,13 @@ module Model
     end
 
 
+    # Checks if user can log in or not
+    #
+    # @option params [String] username The username
+    # @option params [String] password The password
+    #
+    # @return [String] The username of the now logged in account
+    # @return [nil] if credentials do not match a user
     def login_user(username, password)
         db = connect_to_db('db/database.db')
         users_hash = db.execute("SELECT * FROM users WHERE username = ?", username).first
@@ -48,7 +66,14 @@ module Model
         end
     end
     
-
+    # Attempts to insert a new row in the users table
+    #
+    # @option params [String] username The username
+    # @option params [String] password The password
+    # @option params [String] confirmed_password The confirmed password
+    #
+    # @return [String] The name of the registered user
+    # @return [nil] if the confirmed password does not match password 
     def register_user(username, password, confirmed_password)
         db = connect_to_db('db/database.db')
         if confirmed_password == password
@@ -60,7 +85,13 @@ module Model
         end
     end
 
-
+    # Searches for all pokemons that has the specified type 
+    #
+    # @option params [String/nil] type The type of the pokemon user wants displayed. Could also be nil or "all" if user wants to see all pokemons
+    #
+    # @return [Array] containing hashes for each searched pokemons
+        #   * :id [Integer] pokemon id
+        #   * :name [String] the name of the pokemon
     def filter_pokemon(filter)
         db = connect_to_db('db/database.db')
         if filter == "all" || filter == nil
@@ -70,7 +101,15 @@ module Model
         end
     end
 
-
+    # Selects the specified pokemon and its types 
+    #
+    # @param [Integer] pokemon_id The id of specified pokemon
+    #
+    # @return [Array] with pokemon_hash and type_hash_array
+    #   * :id [Integer] pokemon id
+    #   * :name [String] the name of the pokemon
+    #   * :id [Integer] type id
+    #   * :type_name [String] the name of the type
     def get_pokemons_and_types(pokemon_id)
         db = connect_to_db('db/database.db')
     
@@ -80,8 +119,12 @@ module Model
         return [pokemon_hash, type_hash_array]
     end
 
-
-
+    # Inserts new record in team table 
+    #
+    # @param [String] team_name The name of specified team
+    # @param [Array] added_pokemon_id_array The array with the id of which pokemon to add to the team
+    # @param [String] user_name The name of the team owner
+    #
     def new_team(team_name, added_pokemon_id_array, user_name)
         db = connect_to_db('db/database.db')
         user_id = db.execute("SELECT id FROM users WHERE username = ?", user_name).first["id"]
@@ -90,31 +133,52 @@ module Model
         added_pokemon_id_array.each do |added_pokemon_id|
             db.execute("INSERT INTO team_pkmn_relation (team_id, pkmn_id) VALUES (?,?)", team_id, added_pokemon_id)
         end
-        return nil
     end
 
-
+    # Deletes row from team table 
+    #
+    # @param [Integer] team_id The id of a specified team
+    #
     def delete_team(team_id)
         db = SQLite3::Database.new('db/database.db')
         db.execute("DELETE FROM team_pkmn_relation WHERE team_id = ?", team_id)
         db.execute("DELETE FROM team WHERE id = ?", team_id)
     end
 
-
+    # Attempts to update a row in the team table
+    #
+    # @param [Integer] team_id The id of a specified team
+    # @param [String] new_team_name The new name of a specified team
+    #
     def update_team(team_id, new_team_name)
         db = SQLite3::Database.new('db/database.db')
         db.execute("UPDATE team SET team_name = ? WHERE id = ?", new_team_name, team_id)
     end
 
-
-
+    # fetches information from team table 
+    #
+    # @param [Integer] team_id The id of a specified team
+    #
+    # @return [Hash]
+    #   * :id [Integer] team id
+    #   * :team_name [String] the name of the team
+    #   * :user_id [Integer] the id of the user who owns that specific team
     def get_team_hash(team_id)
         db = connect_to_db('db/database.db')
         team_hash = db.execute("SELECT * FROM team WHERE id = ?", team_id).first
         return team_hash
     end
 
-
+    # fetches information about user, team and the pokemon on the teams from the database 
+    #
+    # @param [String] username The name of logged in user
+    #
+    # @return [Array] with other arrays containing information about user, teams and pokemons on the teams
+    #   * :id [Integer] user id
+    #   * :username [String] the name of the logged in user
+    #   * :id [Integer] the id of the team
+    #   * :team_name [String] the name of the team
+    #   * :name [String] the name of a pokemon on a team
     def get_team(username)
         db = connect_to_db('db/database.db')
         if username == "Admin"
@@ -139,7 +203,11 @@ module Model
         return [user_hash_array, team_hash_nested_array, team_pokemon_name_hash_nested_array]
     end
 
-    
+    # fetches username of owner of a specified team
+    #
+    # @param [Integer] team_id The id of a specified team
+    #
+    # @return [String] username
     def get_user_name(team_id)
         db = connect_to_db('db/database.db')
         user_name = db.execute("SELECT username FROM team INNER JOIN users ON users.id = team.user_id WHERE team.id = ?", team_id).first["username"]
